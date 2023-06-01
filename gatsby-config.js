3fe513c0a31a213e533cf2ce92832f59e3b621ff
MartianLee <martionlee@gmail.com>
2023-06-01 23:08:01 +0900

    Install gtag library

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,33 @@ module.exports = {
         trackingId: 'UA-98236060-1',
       },
     },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-M9M7LP1ZFQ', // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: 'OPT_CONTAINER_ID',
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: [],
+          // Delays processing pageview events on route update (in milliseconds)
+          delayOnRouteUpdate: 1,
+        },
+      },
+    },
     'gatsby-plugin-image',
     'gatsby-plugin-react-helmet-async',
     'gatsby-plugin-sitemap',
