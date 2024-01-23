280bacde15ee7dd904140c4df174676c44ebbb49
MartianLee <martionlee@gmail.com>
2024-01-23 16:16:13 +0900

    Update about page contents

---
+++ components/home/Greeting.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Greeting = () => {
+  return (
+    <div className="dark:to-primary mb-8 bg-gradient-to-r from-gray-500 to-slate-400 bg-clip-text text-4xl font-extrabold leading-[60px] tracking-tight text-transparent dark:bg-gradient-to-l dark:from-blue-800 md:text-7xl md:leading-[86px]">
+      Hello, Y'all!{' '}
+      <span className="font-bold">Dive into My Little World of Coding Adventures :)</span>
+    </div>
+  )
+}
+
+export default Greeting
