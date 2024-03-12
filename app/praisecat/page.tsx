213e98ffd483a86de8c07c6ad20120911fbc0373
MartianLee <martionlee@gmail.com>
2024-03-12 19:36:56 +0900

    Upload praisecat developer page

---
+++ app/praisecat/page.tsx
@@ -0,0 +1,27 @@
+import { Authors, allAuthors } from 'contentlayer/generated'
+import { MDXLayoutRenderer } from 'pliny/mdx-components'
+import AuthorLayout from '@/layouts/AuthorLayout'
+import { coreContent } from 'pliny/utils/contentlayer'
+import { genPageMetadata } from 'app/seo'
+import React from 'react'
+
+export const metadata = genPageMetadata({ title: 'About' })
+
+export default function Page() {
+  const author = allAuthors.find((p) => p.slug === 'default') as Authors
+  const mainContent = coreContent(author)
+
+  return (
+    <>
+      <AuthorLayout content={mainContent}>
+        <h1>칭찬할고양 개발자 페이지</h1>
+        <h2>개발자명</h2>
+        <p>martianlee</p>
+        <h2>계정 삭제 요청</h2>
+        <p>martionlee@gmail.com 으로 요청</p>
+        <h2>삭제되거나 보관되는 데이터 유형 및 추가 보관 기간</h2>
+        <p>직전 로그인 후 90일 이내</p>
+      </AuthorLayout>
+    </>
+  )
+}
