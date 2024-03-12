77097d7a1036ec9e25926db0a6addb5682ff76cc
MartianLee <martionlee@gmail.com>
2024-03-12 20:05:23 +0900

    Upload praisecat privacy link

--- app/praisecat/page.tsx
+++ app/praisecat/page.tsx
@@ -4,6 +4,7 @@ import AuthorLayout from '@/layouts/AuthorLayout'
 import { coreContent } from 'pliny/utils/contentlayer'
 import { genPageMetadata } from 'app/seo'
 import React from 'react'
+import Link from '@/components/Link'
 
 export const metadata = genPageMetadata({ title: 'About' })
 
@@ -21,6 +22,16 @@ export default function Page() {
         <p>martionlee@gmail.com 으로 요청</p>
         <h2>삭제되거나 보관되는 데이터 유형 및 추가 보관 기간</h2>
         <p>직전 로그인 후 90일 이내</p>
+        <h2>개인정보처리방침</h2>
+        <p>
+          <Link
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://sites.google.com/view/praisecat-privacy/"
+          >
+            링크에서 확인하기
+          </Link>
+        </p>
       </AuthorLayout>
     </>
   )
