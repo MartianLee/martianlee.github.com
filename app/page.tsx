ec563ca798609249abd05a678c895b46d0b5f863
MartianLee <martionlee@gmail.com>
2025-11-25 21:15:51 +0900

    Update Home contents

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
 import { allBlogs } from '.contentlayer/generated'
 import HeroSection from '@/components/home/HeroSection'
+import ImpactMetricsSection from '@/components/home/ImpactMetricsSection'
 import FeaturedSection from '@/components/home/FeaturedSection'
 import TechStackSection from '@/components/home/TechStackSection'
 import RecentPostsSection from '@/components/home/RecentPostsSection'
@@ -12,6 +13,7 @@ export default async function Page() {
   return (
     <>
       <HeroSection />
+      {/* <ImpactMetricsSection /> */} {/* TODO: update metrics */}
       <FeaturedSection />
       <TechStackSection />
       <RecentPostsSection posts={posts} />
